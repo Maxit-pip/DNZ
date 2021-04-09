b9480a5938d626023576e394a91be8cb90b71afe
Maxit-pip <82233634+Maxit-pip@users.noreply.github.com>
2021-04-09 20:56:46 +0300

    test site

---
+++ app.js
@@ -0,0 +1,12 @@
+VANTA.CLOUDS({
+  el: "#header",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  skyColor: 0x53a6c8,
+  sunColor: 0x894a0f,
+  sunGlareColor: 0x963616,
+  sunlightColor: 0xaa631e
+})
